2dbc7b2cf5b6e2525877a97ed2fcef8599d9fa89
Artem Rhzankov <Artemr2004@rambler.ru>
2024-09-08 10:41:20 +0300

    feat: implement SQL task

---
+++ SQLTask/prepare.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Product(
+                        product_id INT IDENTITY(1, 1) PRIMARY KEY,
+                        name_product VARCHAR(256)
+);
+CREATE TABLE Category(
+                         category_id INT IDENTITY(1, 1) PRIMARY KEY,
+                         name_category VARCHAR(256)
+);
+CREATE TABLE ProductCategory(
+                                id INT IDENTITY(1, 1) PRIMARY KEY,
+                                product_id INT,
+                                category_id INT,
+                                FOREIGN KEY (product_id) REFERENCES Product(product_id),
+                                FOREIGN KEY (category_id) REFERENCES Category(category_id)
+);
---
+++ SQLTask/query.sql
@@ -0,0 +1,4 @@
+﻿SELECT name_product, name_category
+FROM Product
+         LEFT JOIN ProductCategory ON Product.product_id = ProductCategory.product_id
+         LEFT JOIN Category ON Category.category_id = ProductCategory.category_id
